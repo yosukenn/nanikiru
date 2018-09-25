d456031515991190f0b71f848ccf795c8ed9942d
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-25 22:10:22 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180925130050) do
+ActiveRecord::Schema.define(version: 20180925130813) do
 
   create_table "category_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20180925130050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_color_tags_on_name"
+  end
+
+  create_table "coordinates", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.bigint "gender_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gender_id"], name: "index_coordinates_on_gender_id"
+    t.index ["name"], name: "index_coordinates_on_name"
+    t.index ["user_id"], name: "index_coordinates_on_user_id"
   end
 
   create_table "genders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -47,4 +58,6 @@ ActiveRecord::Schema.define(version: 20180925130050) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "coordinates", "genders"
+  add_foreign_key "coordinates", "users"
 end
