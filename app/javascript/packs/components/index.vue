82e0d6eb9792ebafdd4fb3c2437146f2d692a905
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-26 23:13:44 +0900

    edit card layout

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="card-group">
+  <div class="card-deck p-5">
   <div class="card">
     <img class="card-img-top" src="https://smartlog-stat2.imgix.net/uploads/content/piece/2017/4/b34c65e2fe5fd978967bb1d9fda74ac9/20170315130238607_500.jpg?w=500&h=667&auto=compress&q=35" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
