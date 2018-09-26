fa621486c87adb66371624b922c35a88df8dc110
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-26 22:52:16 +0900

    success to appear cards

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="card" style="width: 18rem;">
-    <p>Index</p>
+  <div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="https://smartlog-stat2.imgix.net/uploads/content/piece/2017/4/b34c65e2fe5fd978967bb1d9fda74ac9/20170315130238607_500.jpg?w=500&h=667&auto=compress&q=35" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
   </div>
+  <div class="card">
+    <img class="card-img-top" src="https://smartlog-stat2.imgix.net/uploads/content/piece/2017/4/b34c65e2fe5fd978967bb1d9fda74ac9/20170315130238607_500.jpg?w=500&h=667&auto=compress&q=35" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="https://smartlog-stat2.imgix.net/uploads/content/piece/2017/4/b34c65e2fe5fd978967bb1d9fda74ac9/20170315130238607_500.jpg?w=500&h=667&auto=compress&q=35" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
 </template>
