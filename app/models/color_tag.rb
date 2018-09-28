554732dfd819362d2324df72ed4b80b632f6496f
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-28 15:26:44 +0900

    fix association notion

--- app/models/color_tag.rb
+++ app/models/color_tag.rb
@@ -1,4 +1,4 @@
 class ColorTag < ApplicationRecord
-  has_many :coordinates, through: coordinate_color_tags
+  has_many :coordinates, through: :coordinate_color_tags
   validates :name, presence: true
 end
