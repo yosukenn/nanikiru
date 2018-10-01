c8f727912861e7a8fb730fd8a7f526e9696b47e3
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-01 13:41:33 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181001043221) do
+ActiveRecord::Schema.define(version: 20181001043725) do
 
   create_table "category_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -21,13 +21,6 @@ ActiveRecord::Schema.define(version: 20181001043221) do
     t.index ["name"], name: "index_category_tags_on_name"
   end
 
-  create_table "color_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_color_tags_on_name"
-  end
-
   create_table "coordinate_category_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "coordinate_id", null: false
     t.bigint "category_tag_id", null: false
@@ -35,15 +28,6 @@ ActiveRecord::Schema.define(version: 20181001043221) do
     t.datetime "updated_at", null: false
     t.index ["category_tag_id"], name: "index_coordinate_category_tags_on_category_tag_id"
     t.index ["coordinate_id"], name: "index_coordinate_category_tags_on_coordinate_id"
-  end
-
-  create_table "coordinate_color_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.bigint "coordinate_id", null: false
-    t.bigint "color_tag_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["color_tag_id"], name: "index_coordinate_color_tags_on_color_tag_id"
-    t.index ["coordinate_id"], name: "index_coordinate_color_tags_on_coordinate_id"
   end
 
   create_table "coordinates", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -81,8 +65,6 @@ ActiveRecord::Schema.define(version: 20181001043221) do
 
   add_foreign_key "coordinate_category_tags", "category_tags"
   add_foreign_key "coordinate_category_tags", "coordinates"
-  add_foreign_key "coordinate_color_tags", "color_tags"
-  add_foreign_key "coordinate_color_tags", "coordinates"
   add_foreign_key "coordinates", "genders"
   add_foreign_key "coordinates", "users"
 end
