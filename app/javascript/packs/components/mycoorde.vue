405d45447ce444917a53f01fd2e942a792883878
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-01 18:33:41 +0900

    temporary create coorde registrate form

--- app/javascript/packs/components/mycoorde.vue
+++ app/javascript/packs/components/mycoorde.vue
@@ -1,5 +1,57 @@
 <template>
-  <div>
-    マイコーデ登録
+  <div class="p-5">
+  <form>
+    <div class="form-row">
+
+      <!-- 左部 -->
+      <div class="col">
+        <div class="form-group col-md-12">
+          <input type="email" class="form-control" placeholder="登録したい画像のURLを入力してください">
+        </div>
+        <img class="w-100 px-3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMcQDaIjUOTO7jDRKoMa7ZU6BcFgGGqUlZKKwovb0pHVEJ1gO9" alt="コーディネートイメージ">
+      </div>
+
+      <!-- 右部 -->
+      <div class="col">
+        <div class="form-group col-md-12">
+          <input type="email" class="form-control" placeholder="title">
+        </div>
+        <div class="form-group col-md-6 float-left">
+          <input type="text" class="form-control" placeholder="category">
+        </div>
+        <div class="form-group col-md-6 float-right">
+          <input type="text" class="form-control" placeholder="color">
+        </div>
+        <div class="form-group col-md-6 float-left">
+          <input type="text" class="form-control" placeholder="category">
+        </div>
+        <div class="form-group col-md-6 float-right">
+          <input type="text" class="form-control" placeholder="color">
+        </div>
+        <div class="form-group col-md-6 float-left">
+          <input type="text" class="form-control" placeholder="category">
+        </div>
+        <div class="form-group col-md-6 float-right">
+          <input type="text" class="form-control" placeholder="color">
+        </div>
+        <div class="form-group col-md-6 float-left">
+          <input type="text" class="form-control" placeholder="category">
+        </div>
+        <div class="form-group col-md-6 float-right">
+          <input type="text" class="form-control" placeholder="color">
+        </div>
+        <div class="form-group col-md-6 float-left">
+          <input type="text" class="form-control" placeholder="category">
+        </div>
+        <div class="form-group col-md-6 float-right">
+          <input type="text" class="form-control" placeholder="color">
+        </div>
+      <button type="submit" class="btn btn-primary float-right mr-3">Sign in</button>
+    </div>
+
+    </div>
+
+
+  </form>
   </div>
 </template>
