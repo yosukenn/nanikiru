02315da0b5702b6ad1c1f24601863b4bbb6c13d1
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-01 13:48:54 +0900

    fix search params

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -2,8 +2,8 @@ class CoordinatesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if params[:gender_id] || params[:color_tag] || params[:category_tag]
-      @coordinates = Coordinate.joins(:color_tags, :category_tags).where("gender_id = :gender_id AND color_tags.name = :color_tag AND category_tags.name = :category_tag", {gender_id: params[:gender_id].to_i, color_tag: params[:color_tag], category_tag: params[:category_tag]})
+    if params[:gender_id] && params[:color_tag] && params[:category_tag]
+      @coordinates = Coordinate.joins(:category_tags).where("gender_id = :gender_id AND category_tags.color LIKE :color_tag AND category_tags.name LIKE :category_tag", {gender_id: params[:gender_id].to_i, color_tag: "%#{params[:color_tag]}%", category_tag: "%#{params[:category_tag]}%"})
     else
       @coordinates = Coordinate.order('updated_at DESC')
     end
