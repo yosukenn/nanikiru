8daee9d6cce9d56126eb754f455ec5c39ade8fef
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-06 02:43:48 +0900

    set route for show

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/mycoorde', to: 'coordinates#index'
 
   defaults format: :json do
-    resources :coordinates, only: [:index, :create]
+    resources :coordinates, only: [:index, :create, :show]
     resources :users, only: :index
   end
 end
