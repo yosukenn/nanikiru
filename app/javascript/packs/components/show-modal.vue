b852eeef4dca2ea4071ccfa102abc88798c362e4
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-05 20:18:38 +0900

    fix show_modal layout

--- app/javascript/packs/components/show-modal.vue
+++ app/javascript/packs/components/show-modal.vue
@@ -1,13 +1,16 @@
 <template>
     <transition name="modal">
     <div class="modal-mask">
-      <div class="modal-wrapper modal-dialog modal-dialog-centered">
+      <div class="modal-wrapper modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-container modal-content">
 
           <div class="modal-header">
             <slot name="header">
               <h3>春になったら何着よう</h3>
             </slot>
+            <button class="modal-default-button btn btn-secondary" @click="$emit('close')">
+              x
+            </button>
           </div>
 
           <div class="modal-body">
@@ -18,18 +21,63 @@
                     <img src="https://rr.img.naver.jp/mig?src=http%3A%2F%2Fimgcc.naver.jp%2Fkaze%2Fmission%2FUSER%2F20170119%2F40%2F4513860%2F79%2F323x555xc5c7df4ae9c0000ed4152068.jpg%2F300%2F600&twidth=300&theight=600&qlt=80&res_format=jpg&op=r" alt="コーデイメージ">
                   </div>
                   <div class="col">
-                    aaa
+                    アウター
+                    <div class="row">
+                      <div class="col">
+                        color
+                      </div>
+                      <div class="col">
+                        category
+                      </div>
+                    </div>
+                    トップス
+                    <div class="row">
+                      <div class="col">
+                        color
+                      </div>
+                      <div class="col">
+                        category
+                      </div>
+                    </div>
+                    インナー
+                    <div class="row">
+                      <div class="col">
+                        color
+                      </div>
+                      <div class="col">
+                        category
+                      </div>
+                    </div>
+                    ボトムス
+                    <div class="row">
+                      <div class="col">
+                        color
+                      </div>
+                      <div class="col">
+                        category
+                      </div>
+                    </div>
+                    シューズ
+                    <div class="row">
+                      <div class="col">
+                        color
+                      </div>
+                      <div class="col">
+                        category
+                      </div>
+                    </div>
+                    アクセサリ
+                    <div class="row">
+                      <div class="col">
+                        color
+                      </div>
+                      <div class="col">
+                        category
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </slot>
-          </div>
-
-          <div class="modal-footer">
-            <slot name="footer">
-              <button class="modal-default-button btn btn-secondary" @click="$emit('close')">
-                OK
-              </button>
             </slot>
           </div>
         </div>
