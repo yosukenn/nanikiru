53017b53773e125c31184266798053ddf51a253a
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-06 22:40:55 +0900

    success to appear image but neccesary for resize

--- app/javascript/packs/components/show-modal.vue
+++ app/javascript/packs/components/show-modal.vue
@@ -17,7 +17,7 @@
               <div class="container-fluid">
                 <div class="row">
                   <div class="col">
-                    <img src="https://rr.img.naver.jp/mig?src=http%3A%2F%2Fimgcc.naver.jp%2Fkaze%2Fmission%2FUSER%2F20170119%2F40%2F4513860%2F79%2F323x555xc5c7df4ae9c0000ed4152068.jpg%2F300%2F600&twidth=300&theight=600&qlt=80&res_format=jpg&op=r" alt="コーデイメージ">
+                    <img :src=coordinate.image alt="コーデイメージ">
                   </div>
                   <div class="col">
                     <h5 class="font-weight-bold">アウター</h5>
