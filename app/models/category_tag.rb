e4993efadad2fd051358a22cef1276b143bb8fb9
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-12 17:09:36 +0900

    fix modal to destroy association_modal_instance

--- app/models/category_tag.rb
+++ app/models/category_tag.rb
@@ -1,6 +1,6 @@
 class CategoryTag < ApplicationRecord
   has_many :coordinate_category_tags
-  has_many :coordinates, through: :coordinate_category_tags
+  has_many :coordinates, through: :coordinate_category_tags, dependent: :destroy
   validates :name, presence: true
   validates :color, presence: true
   validates :name, uniqueness: {scope: :color}
