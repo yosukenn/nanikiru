f30b9c467c779cee580c9d0fd759699654722e51
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-23 22:07:38 +0900

    define create action

--- app/controllers/category_tags_controller.rb
+++ app/controllers/category_tags_controller.rb
@@ -2,6 +2,14 @@ class CategoryTagsController < ApplicationController
   protect_from_forgery except: :create
 
   def create
+    coordinate = Coordinate.find(category_tag_params[:id])
+    @category_tag = coordinate.category_tags.build(category_tag_params[:coordinate_item])
+    binding.pry
+    if @category_tag.save
+      render :show, status: :created
+    else
+      render json: @category_tag.errors, status: :unprocessable_entity
+    end
   end
 
   private
