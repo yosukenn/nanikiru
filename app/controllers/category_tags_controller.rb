9f6ce461ffbd83489d997c58a3a4101e9df250be
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-23 22:28:35 +0900

    fix create methods

--- app/controllers/category_tags_controller.rb
+++ app/controllers/category_tags_controller.rb
@@ -4,20 +4,13 @@ class CategoryTagsController < ApplicationController
   def create
     coordinate = Coordinate.find(category_tag_params[:id])
 
-    if @category_tag = CategoryTag.find_by(category_tag_params[:coordinate_item])
-      if coordinate.category_tags << @category_tag
-        render :show, status: :created
-      else
-        render json: @category_tag.errors, status: :unprocessable_entity
-      end
-    elsif @category_tag = CategoryTag.new(category_tag_params[:coordinate_item])
+    if @category_tag = CategoryTag.find_by(category_tag_params[:coordinate_item]) || @category_tag = CategoryTag.new(category_tag_params[:coordinate_item])
       if coordinate.category_tags << @category_tag
         render :show, status: :created
       else
         render json: @category_tag.errors, status: :unprocessable_entity
       end
     end
-    @category_tag = coordinate.category_tags.build(category_tag_params[:coordinate_item])
   end
 
   private
