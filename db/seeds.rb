fceffb7c82ac95c93f11697d18c83fe85cbb107e
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-27 00:58:53 +0900

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+coordinate1 = Coordinate.new(name: 'テーラードジャケットコーデ', image: 'https://o.imgz.jp/fashionnews/106274/kbeigeqaljrbonh.jpg', gender_id: 1, user_id: 1)
+coordinate1.category_tags.build(name: 'テーラードジャケット', color: 'ブラック')
+coordinate1.save
+
+coordinate2 = Coordinate.new(name: 'デニムジャケットコーデ', image: 'https://otokomaeken.com/wp-content/uploads/2018/02/DSC_0128.jpg', gender_id: 1, user_id: 1)
+coordinate2.category_tags.build(name: 'デニムジャケット', color: 'ブルー')
+coordinate2.save
+
+coordinate3 = Coordinate.new(name: 'ミリタリージャケットコーデ', image: 'https://image.t-fashion.jp/coordinates/images/000/017/741/original/267395_1.jpg?1478168836', gender_id: 1, user_id: 1)
+coordinate3.category_tags.build(name: 'ミリタリージャケット', color: 'カーキ')
+coordinate3.save
