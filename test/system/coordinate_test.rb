e1f62e337b30447c906849689f0ea43d631db2d7
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-10-31 18:03:13 +0900

    cetae user but fail

--- test/system/coordinate_test.rb
+++ test/system/coordinate_test.rb
@@ -2,7 +2,8 @@ require "application_system_test_case"
 
 class CoordinatesTest < ApplicationSystemTestCase
   setup do
-    @coordinate = Coordinate.new(name: '定番ジャケット', image: 'https://img1.kakaku.k-img.com/images/tasclapicv/450/000/000/1/1376/tasclapimage_1376_12_1.jpg?d=20160316134944', gender_id: 1, user_id: 1)
+    @user = User.create(nickname: 'john', email: 'john@example.com', password: '000000', password_confirmation: '000000')
+    @coordinate = Coordinate.new(name: '定番ジャケット', image: 'https://img1.kakaku.k-img.com/images/tasclapicv/450/000/000/1/1376/tasclapimage_1376_12_1.jpg?d=20160316134944', gender_id: 1, user_id: @user.id)
     @coordinate.category_tags.build(name: 'ジャケット', color: 'ネイビー')
     @coordinate.save
     visit root_path
